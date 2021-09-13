d6ae8d9db01f9d1f7a5694a19d80ff5081059b47
diiyamoud <vvakhineh@gmail.com>
2021-09-13 10:41:18 +0430

    nla

--- Suckless/dwmblocks/blocks.h
+++ Suckless/dwmblocks/blocks.h
@@ -1,8 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
+	{" ", "/home/diiyamoud/Suckless/dwmblocks/Scripts/cpu_usage", 5, 0},
 	{" ", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
