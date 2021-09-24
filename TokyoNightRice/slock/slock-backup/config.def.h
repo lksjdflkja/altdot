e2102bb635006d54579e35dc92ced09ca3976cc0
Dii <vvakhineh@gmail.com>
2021-09-24 21:35:39 +0330

    ngmi

---
+++ TokyoNightRice/slock/slock-backup/config.def.h
@@ -0,0 +1,21 @@
+/* user and group to drop privileges to */
+static const char *user  = "diiyamoud";
+static const char *group = "wheel";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "black",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Enter the password: ";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x10";
